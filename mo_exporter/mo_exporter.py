babbdc57313c9e00a0dc43dcbe7ad10fe5472799
Zash <zash@fantasymail.de>
2025-09-03 23:45:55 +0200

    Refactor: move dialogs to separate file

--- mo_exporter/mo_exporter.py
+++ mo_exporter/mo_exporter.py
@@ -5,15 +5,13 @@ import zipfile
 from abc import abstractmethod
 from collections.abc import Collection, Iterable, Mapping, Sequence
 from pathlib import Path
-from typing import Any, Callable, Protocol, Self
 
 import mobase
-from PyQt6.QtCore import QDir, Qt
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import (
     QButtonGroup,
     QCheckBox,
-    QFileDialog,
     QGroupBox,
     QHBoxLayout,
     QMessageBox,
@@ -25,59 +23,7 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-
-class OptionsFileDialog(QFileDialog):
-    def with_widgets(self, *widgets: QWidget) -> Self:
-        self.add_widgets(*widgets)
-        return self
-
-    def add_widgets(self, *widgets: QWidget):
-        self.setOption(QFileDialog.Option.DontUseNativeDialog, True)
-        layout = self.layout()
-        assert layout is not None
-        for widget in widgets:
-            layout.addWidget(widget)
-
-    def getDirectory(
-        self,
-        caption: str | None = None,
-        directory: str | None = None,
-        options: QFileDialog.Option = QFileDialog.Option.ShowDirsOnly
-        | QFileDialog.Option.DontResolveSymlinks,
-    ) -> str:
-        self.setFileMode(QFileDialog.FileMode.Directory)
-        self.setFilter(QDir.Filter.Dirs)
-        if caption:
-            self.setWindowTitle(caption)
-        if directory:
-            self.setDirectory(directory)
-        self.setOptions(options)
-        if not self.exec():
-            return ""
-        return self.selectedFiles()[0]
-
-    def getFile(
-        self,
-        caption: str | None = None,
-        directory: str | None = None,
-        filter: str | None = None,
-        initialFilter: str | None = None,
-        options: QFileDialog.Option | None = None,
-    ) -> tuple[str, str]:
-        self.setFileMode(QFileDialog.FileMode.AnyFile)
-        if caption:
-            self.setWindowTitle(caption)
-        if directory:
-            self.setDirectory(directory)
-        if filter:
-            self.setNameFilter(filter)
-        if initialFilter:
-            self.selectNameFilter(initialFilter)
-        if options:
-            self.setOptions(options)
-        if not self.exec():
-            return "", self.selectedNameFilter()
-        return self.selectedFiles()[0], self.selectedNameFilter()
+from .dialogs import FolderExportDialog, OptionsFileDialog
 
 
 class ExporterBase(mobase.IPlugin):
@@ -186,87 +132,6 @@ class ExporterTool(ExporterBase, mobase.IPluginTool):
         if progress:
             progress.setValue(len(mods))
         return paths
-
-
-class HasSettings(Protocol):
-    def get_setting(self, key: str) -> mobase.MoVariant: ...
-    def set_setting(self, key: str, value: mobase.MoVariant): ...
-
-
-class FolderExportDialog:
-    widgets: list[QWidget]
-
-    def __init__(
-        self, settings_plugin: HasSettings, file_dialog: OptionsFileDialog | None
-    ) -> None:
-        self.settings_plugin = settings_plugin
-        self.file_dialog = file_dialog or OptionsFileDialog()
-        self.widgets = []
-        self.add_widget_callbacks(
-            self._add_options(),
-            self._add_export_type(),
-        )
-
-    def add_widget_callbacks(
-        self, *widget_callbacks: tuple[QWidget, Callable[..., Any]]
-    ):
-        widgets: list[QWidget] = []
-        for widget, accept_callback in widget_callbacks:
-            self.widgets.append(widget)
-            widgets.append(widget)
-            self.file_dialog.accepted.connect(accept_callback)  # type: ignore
-        self.file_dialog.add_widgets(*widgets)
-
-    def _add_options(self):
-        # Options
-        options_box = QGroupBox("Options")
-        # overwrite
-        include_overwrite = QCheckBox("Include Overwrite")
-        export_overwrite_setting = self.settings_plugin.get_setting("export-overwrite")
-        if not isinstance(export_overwrite_setting, bool):
-            export_overwrite_setting = False
-        include_overwrite.setChecked(export_overwrite_setting)
-        layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        layout.addWidget(include_overwrite)
-        options_box.setLayout(layout)
-
-        def accept_callback():
-            export_overwrite_setting = include_overwrite.isChecked()
-            self.settings_plugin.set_setting(
-                "export-overwrite", export_overwrite_setting
-            )
-
-        return options_box, accept_callback
-
-    def _add_export_type(self):
-        export_type_box = QGroupBox("Export Type")
-        export_type_group = QButtonGroup()
-        layout = QVBoxLayout()
-        mod_folder_button = QRadioButton("Export mod folders")
-        mod_folder_button.setToolTip("Export each mod as a separate folder")
-        export_type_group.addButton(mod_folder_button)
-        mod_content_button = QRadioButton("Export mod contents")
-        mod_content_button.setToolTip(
-            "Export the contents of each mod together (~virtual file tree)"
-        )
-        export_type_group.addButton(mod_content_button)
-        if self.settings_plugin.get_setting("export-type") == "mod-folder":
-            mod_folder_button.setChecked(True)
-        else:
-            mod_content_button.setChecked(True)
-        for button in export_type_group.buttons():
-            layout.addWidget(button)
-        export_type_box.setLayout(layout)
-
-        def accept_callback():
-            export_contents = mod_content_button.isChecked()
-            self.settings_plugin.set_setting(
-                "export-type",
-                "mod-content" if export_contents else "mod-folder",
-            )
-
-        return export_type_box, accept_callback
 
 
 class FolderExporter(ExporterTool):
